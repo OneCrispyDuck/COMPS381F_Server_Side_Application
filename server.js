53f6ad87a3656b615c6ed6a1296db780ebc45f98
Joey_G <105057850+OneCrispyDuck@users.noreply.github.com>
2024-11-25 16:27:04 +0800

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 const methodOverride = require('method-override');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // MongoDB URI
 const uri = "mongodb+srv://Mike_Wang:Wang090701@cluster.k8uyh.mongodb.net/groupData?retryWrites=true&w=majority&appName=Cluster";
